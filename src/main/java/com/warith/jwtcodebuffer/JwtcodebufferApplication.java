14ae03b88f8f0a9d580b1f6327ad4ca2c3223730
Phakocy <omojolawarith@gmail.com>
2022-04-03 18:49:10 +0100

    commit start

---
+++ src/main/java/com/warith/jwtcodebuffer/JwtcodebufferApplication.java
@@ -0,0 +1,13 @@
+package com.warith.jwtcodebuffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtcodebufferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtcodebufferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/warith/jwtcodebuffer/JwtcodebufferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warith.jwtcodebuffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtcodebufferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
